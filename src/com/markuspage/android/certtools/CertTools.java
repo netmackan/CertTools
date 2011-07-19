a4c08099bdd9395156bf882757cf12eff300f4c4
Markus Kilås <markus@markuspage.com>
2011-07-19 12:35:13 +0200

    Adding license and license headers.

--- src/com/markuspage/android/certtools/CertTools.java
+++ src/com/markuspage/android/certtools/CertTools.java
@@ -1,6 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  Copyright (C) 2011 Markus Kilås
+ * 
+ *  This file is part of CertTools.
+ *
+ *  CertTools is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CertTools is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CertTools.  If not, see <http://www.gnu.org/licenses/>.
+ *  
  */
 package com.markuspage.android.certtools;
 
@@ -21,8 +36,7 @@ import java.util.List;
 import org.bouncyastle.util.encoders.Base64;
 
 /**
- *
- * @author markus
+ * @author Markus Kilås
  */
 public class CertTools {
 
--- src/com/markuspage/android/certtools/CertToolsActivity.java
+++ src/com/markuspage/android/certtools/CertToolsActivity.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2011 Markus Kilås
+ * 
+ *  This file is part of CertTools.
+ *
+ *  CertTools is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CertTools is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CertTools.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ */
 package com.markuspage.android.certtools;
 
 import android.app.ListActivity;
@@ -22,6 +41,10 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * 
+ * @author Markus Kilås
+ */
 public class CertToolsActivity extends ListActivity
 {
     /** Called when the activity is first created. */
